72657df473e521d7e42d5d3f6f9dfb6a599229a4
idogr <ido.greenspan@gmail.com>
2026-06-21 23:32:08 +0300

    example sleeps for 1 second even with injection

--- example/remote.c
+++ example/remote.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
+#include <errno.h>
+
+void exact_sleep_seconds(int seconds);
 
 int main()
 {
@@ -8,6 +12,31 @@ int main()
     while (1)
     {
         puts("running in a loop");
-        sleep(1);
+        exact_sleep_seconds(1);
+    }
+}
+
+void exact_sleep_seconds(int seconds)
+{
+    struct timespec req, rem;
+
+    // Set initial sleep duration to 1 second, 0 nanoseconds
+    req.tv_sec = seconds;
+    req.tv_nsec = 0;
+
+    // Loop until nanosleep succeeds without being interrupted by a signal
+    while (nanosleep(&req, &rem) == -1)
+    {
+        if (errno == EINTR)
+        {
+            // Interrupted by a signal! Update req with the remaining time and try again.
+            req = rem;
+        }
+        else
+        {
+            // A different error occurred (rare for nanosleep, but good practice to handle)
+            perror("nanosleep failed");
+            break;
+        }
     }
 }
